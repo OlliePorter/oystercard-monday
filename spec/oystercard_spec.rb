63d2c2678dfe5484169216ed769ab1a9f2b2cf88
Ollie Porter <ol.porter@me.com>
2019-05-20 15:16:09 +0100

    Now initializing an oystercard balane

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,8 @@
 require 'oystercard'
 
 describe Oystercard do
-  
+  it 'expects new oystercard to have a balance of 0' do
+    oystercard = Oystercard.new
+    expect(oystercard.balance).to eq(0)
+  end
 end
