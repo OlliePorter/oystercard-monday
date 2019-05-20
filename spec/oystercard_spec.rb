b2dc2d6afb0db30909fbc91322ba641012bd118d
Geoff Smith <smith260194@gmail.com>
2019-05-20 15:22:18 +0100

    Adding top up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,4 +5,12 @@ describe Oystercard do
     oystercard = Oystercard.new
     expect(oystercard.balance).to eq(0)
   end
+
+  it 'can increase the balance if top-up is called' do
+    oystercard = Oystercard.new
+
+    oystercard.top_up(5)
+
+    expect(oystercard.balance).to eq(5)
+  end
 end
